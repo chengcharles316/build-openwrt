44ee3c9e10a52e23a2128fb5622ca80d8d5d50e3
chengcharles316 <67448932+chengcharles316@users.noreply.github.com>
2021-08-16 11:30:33 +0800

    Update diy-2.sh

--- build/Lede_redmi_ac2100/diy-2.sh
+++ build/Lede_redmi_ac2100/diy-2.sh
@@ -12,7 +12,7 @@ sed -i '/uci commit system/i\uci set system.@system[0].hostname='Redmi AC2100'' 
 sed -i "s/OpenWrt /RMIop build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
-sed -i 's/luci-theme-bootstrap/luci-theme-infinityfreedom/g' feeds/luci/collections/luci/Makefile
+#sed -i 's/luci-theme-bootstrap/luci-theme-infinityfreedom/g' feeds/luci/collections/luci/Makefile
 
 # 设置路由器登陆密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
